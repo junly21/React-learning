9a891a8f59c6a39fcc1c830ab6e363981b5a2957
Jaejoon <jaejoon0721@ajou.ac.kr>
2023-01-23 18:45:14 +0900

    4장 input과 btn target설정해주기(input이 여러개일때)

--- 04/hello-react/src/diy-EventPractice.js
+++ 04/hello-react/src/diy-EventPractice.js
@@ -1,28 +1,51 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 
-class EventPractice extends Component {
-  state = {
-    message: 'testing',
+const EventPractice = () => {
+  const [form, setForm] = useState({
+    username: '',
+    message: '',
+  });
+  const { username, message } = form;
+  const onChange = (e) => {
+    setTimeout(() => console.log(e), 500);
+    const nextForm = {
+      ...form, // 기존의 form 내용을 이 자리에 복사 한 뒤
+      [e.target.name]: e.target.value, // 원하는 값을 덮어씌우기
+    };
+    setForm(nextForm);
   };
-
-  render() {
-    const handleChange = (e) => {
-      this.setState({ message: e.target.value });
-    };
-    const handleClick = (e) => {
-      alert(this.state.message);
-    };
-
-    return (
-      <div>
-        <input
-          placeholder="dfdfd"
-          value={this.state.message}
-          onChange={handleChange}
-        ></input>
-        <button onClick={handleClick}>btn</button>
-      </div>
-    );
-  }
-}
+  const onClick = () => {
+    alert(username + ': ' + message);
+    setForm({
+      username: '',
+      message: '',
+    });
+  };
+  const onKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      onClick();
+    }
+  };
+  return (
+    <div>
+      <h1>이벤트 연습</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="유저명"
+        value={username}
+        onChange={onChange}
+      />
+      <input
+        type="text"
+        name="message"
+        placeholder="아무거나 입력해보세요"
+        value={message}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+      <button onClick={onClick}>확인</button>
+    </div>
+  );
+};
 export default EventPractice;
