aac2ef3c35b4e39d6b49686ffe51535b709e17b4
Jaejoon <jaejoon0721@ajou.ac.kr>
2023-02-03 01:30:39 +0900

    API호출하여 News 사이트 만들기

---
+++ 14/news-viewer/src/JoonComponents/NewsList.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          'https://newsapi.org/v2/top-headlines?country=kr&category=technology&apiKey=32c8771d847d44599f381f5635e65132',
+        );
+
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <NewsListBlock>대기중...</NewsListBlock>;
+  }
+  if (!articles) {
+    return null;
+  }
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
