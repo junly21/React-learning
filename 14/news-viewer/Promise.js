0cca4c327bcbf125c37473525c971f1e068bbe51
Jaejoon <jaejoon0721@ajou.ac.kr>
2023-02-02 22:09:04 +0900

    Promise

---
+++ 14/news-viewer/Promise.js
@@ -0,0 +1,38 @@
+function increase(number) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = number + 10;
+      if (result > 50) {
+        const e = new Error("50을 초과했다.");
+        return reject(e);
+      }
+      resolve(result);
+    }, 1000);
+  });
+  return promise;
+}
+
+increase(0)
+  .then((result) => {
+    console.log(result);
+    return increase(result);
+  })
+  .then((num) => {
+    console.log(num);
+    return increase(num);
+  })
+  .then((again) => {
+    console.log(again);
+    return increase(again);
+  })
+  .then((again2) => {
+    console.log(again2);
+    return increase(again2);
+  })
+  .then((again3) => {
+    console.log(again3);
+    return increase(again3);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
