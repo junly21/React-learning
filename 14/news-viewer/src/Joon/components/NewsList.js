930efd9c1caf8a75e1517836bc43041fd2b2b410
Jaejoon <jaejoon0721@ajou.ac.kr>
2023-02-07 18:40:16 +0900

    6일 커밋 안한부분 뉴스 완성

--- 14/news-viewer/src/Joon/components/NewsList.js
+++ 14/news-viewer/src/Joon/components/NewsList.js
@@ -16,7 +16,7 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -24,8 +24,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
+        const query = category === 'all' ? '' : `&category=${category}`;
         const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&category=technology&apiKey=32c8771d847d44599f381f5635e65132',
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=32c8771d847d44599f381f5635e65132`,
         );
 
         setArticles(response.data.articles);
@@ -35,7 +36,7 @@ const NewsList = () => {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [category]);
 
   if (loading) {
     return <NewsListBlock>대기중...</NewsListBlock>;
